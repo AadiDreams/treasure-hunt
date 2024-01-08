aec18d6aa47f97ed002f7d3a5a9ab00b5a7a79d5
Aadithya V Anand <aadithyadreams@gmail.com>
2024-01-09 00:06:17 +0530

    separate file

---
+++ script.js
@@ -0,0 +1,9 @@
+function check(){
+    const value=document.getElementById("code").value;
+    if(value==754893){
+        window.location.href="./two.html";
+    }
+    else{
+        window.alert("incorrect code")
+    }
+}
